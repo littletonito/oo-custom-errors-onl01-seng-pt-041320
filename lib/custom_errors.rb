26e358fed36802b43658431298952541a5202c1c
Antonio Clavijo Cortes <littletonito@gmail.com>
2020-05-21 19:48:32 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -10,7 +10,9 @@ class Person
     if person.class != Person
       begin
         raise PartnerError
-      rescue
+      rescue PartnerError => error 
+        puts error.message
+      end
     else
       person.partner = self
     end
