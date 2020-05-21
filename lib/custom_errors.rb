54e3dd1061d9b674d56d6e1a4b1dc18441d1f187
Antonio Clavijo Cortes <littletonito@gmail.com>
2020-05-21 19:33:10 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -8,7 +8,9 @@ class Person
   def get_married(person)
     self.partner = person
     if person.class != Person
-    person.partner = self
+      raise PartnerError
+    else
+      person.partner = self
   end
   
   class PartnerError < StandardError
